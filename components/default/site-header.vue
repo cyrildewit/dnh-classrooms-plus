9a28cf500b8333ffbe927d621a788042a6c733f1
Cyril de Wit <cyrilwit@gmail.com>
2020-03-27 14:04:41 +0100

    feat: wip

---
+++ components/default/site-header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-white">
+    <div class="container mx-auto px-4 py-8">
+  <h1>test</h1>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'site-header',
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+      <site-header></site-header>
+
+      <nuxt/>
+
+  </div>
+</template>
+
+<script>
+import SiteHeader from '../components/default/site-header';
+
+export default {
+  components: {
+    SiteHeader,
+  }
+}
+</script>
