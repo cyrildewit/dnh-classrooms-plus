78917f8e8ce99af10e2a2be40c785f8c5ddd8244
Cyril de Wit <cyrilwit@gmail.com>
2020-03-27 13:12:21 +0100

    feat: wip

--- pages/login.vue
+++ pages/login.vue
@@ -3,18 +3,19 @@
         <h1>Login page</h1>
 
         <form @submit.prevent="login">
-            <input type="text" v-model="user.email" placeholder="E-mail">
+            <input type="text" v-model="user.email" placeholder="E-mail" class="text-xl font-sans border-2">
             <br>
-            <input type="text" v-model="user.password" placeholder="Password">
+            <input type="text" v-model="user.password" placeholder="Password" class="text-xl font-sans border-2">
 
-            <button>Login</button>
+            <button class="bg-blue-500 px-2 py-3 rounded text-white">Login</button>
         </form>
-
     </div>
 </template>
 
 <script>
 export default {
+  layout: 'default',
+
     data: () => ({
         user: {
             email: null,
